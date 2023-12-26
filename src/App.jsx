218193a665d4f066b056bd02daba8d0db5c3eb21
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2023-12-26 13:49:36 +0200

    Initialized and cloned repository, set up project structure. Ready to code!  #ReactCalculator

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./assets/styles/main.style/main.scss";
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore nostrum
+      quis adipisci repudiandae, distinctio beatae ipsum ea at libero maxime
+      odio laborum sed, cumque explicabo velit optio? Omnis, quaerat qui.
+    </div>
+  );
+}
+
+export default App;
